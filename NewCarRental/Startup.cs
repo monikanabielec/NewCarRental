f7bc76e50a10db1278c7129026b1e93733bb1e33
Monia <monika.nabielec@gmail.com>
2018-12-21 20:45:06 +0100

    Add project files.

---
+++ NewCarRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewCarRental.Startup))]
+namespace NewCarRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
